0ff93e1f9819be92cbb966f35dd4e2a1c7624147
ila36IX <jbariali002@gmail.com>
2024-08-28 03:15:46 +0100

    Making the navbar mobile friendly

--- client/src/scenes/AsideNav.jsx
+++ client/src/scenes/AsideNav.jsx
@@ -1,39 +1,34 @@
 import { useState } from 'react';
-import { FaUserFriends } from "react-icons/fa";
-import { FaRobot } from "react-icons/fa6";
-import { TfiCup } from "react-icons/tfi";
-import { MdLeaderboard } from "react-icons/md";
-import { FaInfoCircle } from "react-icons/fa";
-
-const icon = (
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		viewBox="0 0 24 24"
-		fill="currentColor"
-		className="size-6"
-	>
-		<path d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75ZM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 0 1-1.875-1.875V8.625ZM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 0 1 3 19.875v-6.75Z" />
-	</svg>
-);
-
-const navigation = [
-	{ name: "Play vs friend", href: '#', icon: <FaUserFriends />, current: true },
-	{ name: 'Play vs robot', href: '#', icon: <FaRobot />, current: false },
-	{ name: 'LeaderBoard', href: '#', icon: <MdLeaderboard />, current: false },
-	{ name: 'About', href: '#', icon: <FaInfoCircle />, current: false },
-];
+import navigation from '../components/navigationMetaData';
+import { Link } from 'react-router-dom';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
+import { FaCircleArrowRight, FaCircleArrowLeft } from "react-icons/fa6";
 
 function classNames(...classes) {
 	return classes.filter(Boolean).join(' ');
 }
 
-export default function AsideNav({ children }) {
+export default function AsideNav({ children, title }) {
 	const [sidebarOpen, setSidebarOpen] = useState(false);
+	const [navPage, setNavPage] = useState(navigation);
 	const [expand, setExpand] = useState(true);
+
+	function updateCurrentPage(id) {
+		const updatedPages = [...navPage].map(page => {
+			if (page.id === id) {
+				page.current = true;
+			} else {
+				page.current = false;
+			}
+			return page;
+		});
+		setNavPage(updatedPages);
+	}
 
 	return (
 		<div>
-			<div className={`hidden md:fixed md:inset-y-0 md:flex md:${expand ? 'w-64' : 'w-16'} ${expand ? 'w-64' : 'w-16'}`}>
+			<div className={`hidden md:fixed md:inset-y-0 md:flex ${expand ? 'md:w-64' : 'md:w-16'} `}>
 				<div className="flex min-h-0 flex-1 flex-col bg-primary">
 					<div className="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
 						<div className={`flex flex-shrink-0 items-center px-${expand ? '4' : '2'}`}>
@@ -44,27 +39,28 @@ export default function AsideNav({ children }) {
 							/>
 						</div>
 						<nav className={`flex-1 space-y-1 px-2 ${expand ? 'mt-0' : 'mt-16'}`}>
-							{navigation.map((item) => (
+							{navPage.map((item) => (
 								<>
 
-									<a
-										key={item.name}
-										href={item.href}
+									<Link
+										to={item.href}
 										className={classNames(
 											item.current ? 'bg-secondaryLight text-white' : 'text-white hover:bg-primaryLighter hover:bg-opacity-75',
 											'group flex gap-2 px-2 py-2 text-xl font-bold items-center', expand ? '' : 'justify-center'
 										)}
+										onClick={() => updateCurrentPage(item.id)}
+										kay={item.id}
 									>
-										{<div class='text-2xl'>{item.icon}</div>}
+										{<div className='text-2xl'>{item.icon}</div>}
 										{expand ? item.name : ''}
-									</a>
+									</Link>
 								</>
 
 							))}
 						</nav>
-					</div >
+					</div>
 					<div className={`flex border-t-2 flex-shrink-0 border-secondary ${expand ? 'p-3' : 'p-1'}`}>
-						<a href="#" className="group block w-full flex-shrink-0 hover:bg-primaryLight p-1">
+						<Link to="/profile" className="group block w-full flex-shrink-0 hover:bg-primaryLight p-1">
 							<div className="flex items-center">
 								<div>
 									<img
@@ -80,31 +76,73 @@ export default function AsideNav({ children }) {
 									</div>
 								) : null}
 							</div>
-						</a>
+						</Link>
 					</div>
 				</div>
 				<button
-					className="absolute bg-primary text-white text-bold top-1/2 right-[-20px] rounded w-8 h-8"
+					className="absolute bg-primary rounded-full text-white hover:bg-primaryLighter text-bold top-1/2 right-[-20px] rounded p-3"
 					onClick={() => setExpand(!expand)}
 				>
-					{expand ? '<<' : '>>'}
+					{expand ? <FaCircleArrowLeft /> : <FaCircleArrowRight />}
 				</button>
 			</div>
-			<div className={`flex flex-1 flex-col w-100 md:${expand ? 'pl-64' : 'pl-16'} ${expand ? 'pl-64' : 'pl-16'}`}>
-				<div className="sticky top-0 z-10 bg-gray-100 pl-1 pt-1 sm:pl-3 sm:pt-3 md:hidden">
+			<div className={`flex flex-1 flex-col w-100 `}>
+				<div className="sticky flex top-0 z-10 bg-primary py-2 px-5 md:hidden justify-between items-center">
+
+					<div className={`flex flex-shrink-0`}>
+						<a href="#" className="group block flex-shrink-0 hover:bg-primaryLight p-1">
+							<div className="flex items-center ">
+								<div>
+									<img
+										className="inline-block w-14 rounded-full"
+										src="/user_avatar1.jpg"
+										alt=""
+									/>
+								</div>
+
+								<div className="ml-3">
+									<p className="text-lg font-mono text-white">alien</p>
+									<p className="text-xs text-white group-hover:text-white">View profile</p>
+								</div>
+
+							</div>
+						</a>
+					</div>
+
 					<button
 						type="button"
-						className="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-						onClick={() => setSidebarOpen(true)}
+						className="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center text-3xl justify-center bg-primaryLight text-white hover:bg-primaryLighter focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary"
+						onClick={() => setSidebarOpen(!sidebarOpen)}
 					>
 						<span className="sr-only">Open sidebar</span>
-						x
+						{sidebarOpen ? <IoClose /> : <GiHamburgerMenu />}
 					</button>
 				</div>
-				<main className="flex-1 min-h-screen bg-primaryLight">
+				{/* mobile nav */}
+				{sidebarOpen ? <div className='sticky top-14 sm:hidden bg-primary px-5 pb-5 border-t-2'>
+					<nav className='flex-1 space-y-1 px-2 mt-5'>
+						{navPage.map((item) => (
+							<>
+								<Link
+									to={item.href}
+									className={classNames(
+										item.current ? 'bg-secondaryLight text-white' : 'text-white hover:bg-primaryLighter hover:bg-opacity-75',
+										'group flex gap-2 px-2 py-2 text-xl items-center'
+									)}
+									onClick={() => updateCurrentPage(item.id)}
+									kay={item.id}
+								>
+									{item.icon}
+									{item.name}
+								</Link>
+							</>
+						))}
+					</nav>
+				</div> : null}
+				<main className={`flex-1 min-h-screen bg-primaryLight md:${expand ? 'pl-64' : 'pl-16'}`}>
 					<div className="py-6">
 						<div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8 mb-5">
-							<h1 className="text-4xl font-semibold text-white">Leaderboard</h1>
+							<h1 className="text-4xl font-semibold text-white">{title}</h1>
 						</div>
 						<div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
 							<div className="py-4">
