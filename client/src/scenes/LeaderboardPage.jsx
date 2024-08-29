9eb6e2d80add090f5389a693f8003137bc41915e
ila36IX <jbariali002@gmail.com>
2024-08-29 07:18:11 +0100

    Making the board page and leaderboard responsive

--- client/src/scenes/LeaderboardPage.jsx
+++ client/src/scenes/LeaderboardPage.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import Leaderboard from "../components/leaderboard.jsx";
 import AsideNav from "./AsideNav.jsx";
 
-const LeaderboardPage = () => {
-    return (
-        <div>
-            <Leaderboard/>
-        </div>
-    )
+const LeaderboardPage = ({ setTitle }) => {
+    setTitle('Meet the Goats!');
+    return <>
+        <Leaderboard />
+    </>
 }
 
 export default LeaderboardPage;
