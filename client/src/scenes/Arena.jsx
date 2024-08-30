b6a7e011cd49b7d891f767785a3816b72064869c
Badr_Annabi <badrannabi@gmail.com>
2024-08-30 21:37:32 +0100

    respect screen size

--- client/src/scenes/Arena.jsx
+++ client/src/scenes/Arena.jsx
@@ -171,34 +171,36 @@ function Arena({setTitle}) {
     [newMessage, myTurn]
   );
 
-  return (
+
+
+return (
     <div className="min-h-screen w-full bg-primary flex items-center justify-center p-4">
-      <div className="flex flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-8 w-full max-w-6xl">
+      <div className="flex flex-col lg:flex-row justify-center items-center space-y-8 md:space-y-0 md:space-x-8 w-full max-w-6xl">
         <div className="relative bg-primaryLight bg-opacity-40 backdrop-blur-lg p-8 rounded-lg shadow-lg flex-1 flex flex-col items-center justify-center">
           {playerId ? (
-            <div className="relative grid grid-cols-3 grid-rows-3 gap-2 w-[400px] h-[400px] border border-primaryLight bg-primaryLighter bg-opacity-20 backdrop-blur-md">
-              {Array(9)
-                .fill(null)
-                .map((_, index) => renderSquare(index))}
-            </div>
+              <div className="relative grid grid-cols-3 grid-rows-3 gap-2 w-[400px] h-[400px] border border-primaryLight bg-primaryLighter bg-opacity-20 backdrop-blur-md">
+                {Array(9)
+                    .fill(null)
+                    .map((_, index) => renderSquare(index))}
+              </div>
           ) : (
-            <RotatingMsg />
+              <RotatingMsg />
           )}
           {(winner !== null || isDrow) && (
-            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-2xl font-bold">
-              {isDrow ? 'DRAW' : winner ? 'You are a winner' : 'Looooooser'}
-            </div>
+              <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-2xl font-bold">
+                {isDrow ? 'DRAW' : winner ? 'You are a winner' : 'Looooooser'}
+              </div>
           )}
-          <div className="absolute bottom-4 left-4 right-4 flex justify-between">
+          <div className="absolute  bottom-4 left-4 right-4 flex justify-between">
             <button
-              onClick={handlePlayAgain}
-              className="px-4 py-2 bg-secondary text-white rounded-md shadow-md hover:bg-secondaryLight"
+                onClick={handlePlayAgain}
+                className="px-4 py-2 bg-secondary text-white rounded-md shadow-md hover:bg-secondaryLight"
             >
               Play Again
             </button>
             <button
-              onClick={handleLeave}
-              className="px-4 py-2 bg-red-500 text-white rounded-md shadow-md hover:bg-red-600"
+                onClick={handleLeave}
+                className="px-4 py-2 bg-red-500 text-white rounded-md shadow-md hover:bg-red-600"
             >
               Leave
             </button>
@@ -209,37 +211,37 @@ function Arena({setTitle}) {
           <div className="text-white font-bold mb-4">Chat Board</div>
           <div className="flex-1 overflow-y-auto mb-4">
             {messages.map((msg, index) => (
-              <div
-                key={index}
-                className={`flex ${
-                  msg.fromMe ? 'justify-end' : 'justify-start'
-                } mb-2`}
-              >
                 <div
-                  className={`px-4 py-2 rounded-lg ${
-                    msg.fromMe
-                      ? 'bg-secondary text-white'
-                      : 'bg-white text-primary'
-                  }`}
+                    key={index}
+                    className={`flex ${
+                        msg.fromMe ? 'justify-end' : 'justify-start'
+                    } mb-2`}
                 >
-                  {msg.text}
+                  <div
+                      className={`px-4 py-2 rounded-lg ${
+                          msg.fromMe
+                              ? 'bg-secondary text-white'
+                              : 'bg-white text-primary'
+                      }`}
+                  >
+                    {msg.text}
+                  </div>
                 </div>
-              </div>
             ))}
           </div>
           <div className="flex">
             <input
-              type="text"
-              value={newMessage}
-              onChange={(e) => setNewMessage(e.target.value)}
-              onKeyDown={handleKeyDown}
-              placeholder="Type a message..."
-              className="flex-1 px-4 py-2 border rounded-lg"
+                type="text"
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+                onKeyDown={handleKeyDown}
+                placeholder="Type a message..."
+                className="flex-1 px-4 py-2 border rounded-lg"
             />
             <button
-              type="submit"
-              onClick={handleSubmitMessage}
-              className="px-4 py-2 bg-secondary text-white rounded-lg ml-2"
+                type="submit"
+                onClick={handleSubmitMessage}
+                className="px-4 py-2 bg-secondary text-white rounded-lg ml-2"
             >
               Send
             </button>
@@ -248,29 +250,28 @@ function Arena({setTitle}) {
       </div>
 
       {showPopup && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-8 rounded-lg shadow-lg">
-            <h2 className="text-xl font-bold mb-4">Confirm Leave</h2>
-            <p className="mb-4">Are you sure you want to leave the game?</p>
-            <div className="flex justify-end">
-              <button
-                onClick={handleCancelLeave}
-                className="px-4 py-2 bg-gray-300 rounded-lg mr-2"
-              >
-                Cancel
-              </button>
-              <button
-                onClick={handleConfirmLeave}
-                className="px-4 py-2 bg-red-500 text-white rounded-lg"
-              >
-                Leave
-              </button>
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-white p-8 rounded-lg shadow-lg">
+              <h2 className="text-xl font-bold mb-4">Confirm Leave</h2>
+              <p className="mb-4">Are you sure you want to leave the game?</p>
+              <div className="flex justify-end">
+                <button
+                    onClick={handleCancelLeave}
+                    className="px-4 py-2 bg-gray-300 rounded-lg mr-2"
+                >
+                  Cancel
+                </button>
+                <button
+                    onClick={handleConfirmLeave}
+                    className="px-4 py-2 bg-red-500 text-white rounded-lg"
+                >
+                  Leave
+                </button>
+              </div>
             </div>
           </div>
-        </div>
       )}
     </div>
-  );
+)
 }
-
 export default Arena;
