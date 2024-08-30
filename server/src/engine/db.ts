21a59f9b273716dc302990fbc01a55f47513ab15
RadouaneAbn <radouaneabn@gmail.com>
2024-08-30 17:30:52 +0100

    use local db

--- server/src/engine/db.ts
+++ server/src/engine/db.ts
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-import 'dotenv/config'
+// import 'dotenv/config'
 
-mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.p4v1m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+mongoose.connect(`mongodb://localhost:27017/tic-tac-toe`).then(() => {
   console.log('db is connected');
 }).catch(() => {
   console.log('couldn\'t connect');
 });
+
+// mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.p4v1m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+//   console.log('db is connected');
+// }).catch(() => {
+//   console.log('couldn\'t connect');
+// });
 
 export default mongoose;
