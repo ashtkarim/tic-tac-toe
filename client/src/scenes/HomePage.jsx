8f6d4ed14706c4eb06bac6db7b3ca6e42f92d162
Badr_Annabi <badrannabi@gmail.com>
2024-08-30 01:57:35 +0100

    add stars in the bg

--- client/src/scenes/HomePage.jsx
+++ client/src/scenes/HomePage.jsx
@@ -15,17 +15,39 @@ function HomePage({ setTitle }) {
   setTitle('Welcome on the tic tac toe game!');
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2">
-      <div className="flex flex-col">
-        <div className='px-14 md:px-32 mb-16'>
-          <img src="/brand_logo.svg" />
-        </div>
-        <Link to="/login" className='w-full text-center font-bold text-xl border border-secondary bg-primary rounded-full py-2 text-white hover:bg-primaryLighter focus:border-white'>Let's play</Link>
+      <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className="floating-stars">
+              {[...Array(50)].map((_, index) => (
+                  <div
+                      key={index}
+                      className= {`star ${Math.random() >= 0.5 ? 'text-secondary' : 'text-white'}`}
+                      symbol = {`${Math.random() >= 0.5 ? 'x' : 'o'}`}
+
+                  style={{
+                          fontSize: `${Math.random() * 28 + 2}px`,
+
+                          // height: `${Math.random() * 5 + 2}px`,
+                          top: `${Math.random() * 100}vh`,
+                          left: `${Math.random() * 100}vw`,
+                          animationDuration: `${Math.random() * 10 + 5}s`,
+                          animationDelay: `${Math.random() * 10}s`,
+                          filter: `blur(${Math.random() * 2}px)`,
+                      }}
+                  ></div>
+              ))}
+          </div>
+          <div className="flex flex-col">
+              <div className='px-14 md:px-32 mb-16'>
+                  <img src="/brand_logo.svg"/>
+              </div>
+              <Link to="/signin"
+                    className='w-full text-center font-bold text-xl border border-secondary bg-primary rounded-full py-2 text-white hover:bg-primaryLighter focus:border-white'>Let's
+                  play</Link>
+          </div>
+          <div className="relative flex mt-14 md:block hidden">
+              <CardsBrand/>
+          </div>
       </div>
-      <div className="relative flex mt-14 md:block hidden">
-        <CardsBrand />
-      </div>
-    </div >
   )
 }
 
